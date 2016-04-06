5e67e6186e5ac0f8ba873d13935261c453e5a5e0
Adil Ansari <ansari.adil20@gmail.com>
2016-04-06 00:34:27 -0700

    working post

--- runserver.py
+++ runserver.py
@@ -11,9 +11,9 @@ CHANNEL_NAME = 'notifications'
 NOTIFICATION_EVENT_NAME = 'new_notification'
 
 pusher = Pusher(
-    app_id=os.environ['PUSHER_APP_ID'],
-    key=os.environ['PUSHER_KEY'],
-    secret=os.environ['PUSHER_SECRET']
+    app_id=os.environ.get('PUSHER_APP_ID', '194612'),
+    key=os.environ.get('PUSHER_KEY', 'f4ee88e008548564dbc5'),
+    secret=os.environ.get('PUSHER_SECRET', '920f9448c42e8ff5c49c')
 )
 
 
@@ -24,6 +24,8 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def login():
+    username = request.form['username']
+    print 'logging in {}'.format(username)
     return redirect(url_for('chat'))
 
 
@@ -31,14 +33,18 @@ def login():
 def chat():
     return render_template(
         'chat.html',
-        pusher_key=os.environ['PUSHER_KEY'],
+        pusher_key=os.environ.get('PUSHER_KEY', 'f4ee88e008548564dbc5'),
         channel_name=CHANNEL_NAME,
         notification_event_name=NOTIFICATION_EVENT_NAME)
 
 
-@app.route('/publish')
-def pixel():
-    print 'pixel'
+@app.route('/publish', methods=['POST'])
+def publish():
+    message = request.form['message']
+    pusher.trigger(CHANNEL_NAME, NOTIFICATION_EVENT_NAME, {'message': message})
+    response = jsonify(message='Message sent')
+    response.status_code = 200
+    return response
 
 
 if __name__ == '__main__':
