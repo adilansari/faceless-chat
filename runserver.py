0adfc76dc93273e13921e68b16e5407b61f0c20b
Adil Ansari <adil@chartboost.com>
2016-04-06 20:25:28 -0700

    better session management

--- runserver.py
+++ runserver.py
@@ -1,6 +1,5 @@
 from flask import *
 from pusher import Pusher
-import time
 import os
 
 app = Flask(__name__)
@@ -19,20 +18,27 @@ pusher = Pusher(
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    if is_logged_in():
+        return redirect(url_for('chat.html'))
+    else:
+        return render_template('login.html')
 
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
-    print 'logging in {}'.format(username)
+    login_user(username)
     return redirect(url_for('chat'))
 
 
 @app.route('/chat')
 def chat():
+    if not is_logged_in():
+        return redirect(url_for('home'))
+
     return render_template(
         'chat.html',
+        username=get_username(),
         pusher_key=os.environ.get('PUSHER_KEY', 'f4ee88e008548564dbc5'),
         channel_name=CHANNEL_NAME,
         notification_event_name=NOTIFICATION_EVENT_NAME)
@@ -41,11 +47,32 @@ def chat():
 @app.route('/publish', methods=['POST'])
 def publish():
     message = request.form['message']
-    pusher.trigger(CHANNEL_NAME, NOTIFICATION_EVENT_NAME, {'message': message})
+    pusher.trigger(CHANNEL_NAME, NOTIFICATION_EVENT_NAME, {'username': get_username(), 'message': message})
     response = jsonify(message='Message sent')
     response.status_code = 200
     return response
 
 
+@app.route('/logout', methods=['POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+def is_logged_in():
+    return 'username' in session
+
+def get_username():
+    return session['username']
+
+def login_user(username):
+    print 'logging in {}'.format(username)
+    session['username'] = username
+
+
+def logout_user():
+    session.pop('username')
+
 if __name__ == '__main__':
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(host='0.0.0.0', debug=True)
